163c3f49a6afbc33be450d277e0d23be5c7422ab
XH·HlulzbinbashH·HX <112975863+lulzbinbashspider@users.noreply.github.com>
2026-06-26 00:55:56 +0200

    Update module header and author information

--- modules/Token.py
+++ modules/Token.py
@@ -2,12 +2,12 @@
 # -*- coding: utf-8 -*-
 
 #-:-:-:-:-:-:-::-:-:#
-#    XSRF Probe     #
+#   Lulz-CSRF     #
 #-:-:-:-:-:-:-::-:-:#
 
-# Author: 0xInfection
-# This module requires XSRFProbe
-# https://github.com/0xInfection/XSRFProbe
+#Author: kroketon lulzbin
+#This module requires Lulz-CSRF
+#https://github.com/lulzbinbashspider/Lulz-CSRF
 
 from files import config
 from re import search, I
