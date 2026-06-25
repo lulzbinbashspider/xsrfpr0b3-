bda89197d80c81d05487855cc64a548eb8946a46
XH·HlulzbinbashH·HX <112975863+lulzbinbashspider@users.noreply.github.com>
2026-06-26 01:20:08 +0200

    Update and rename xsrfkr0k3.py to Lulz-CSRF.py

--- Lulz-CSRF.py
+++ Lulz-CSRF.py
@@ -2,11 +2,11 @@
 # -*- coding: utf-8 -*-
 
 #-:-:-:-:-:-:-::-:-:#
-#    XSRF kroke     #
+#    Lulz-CSRF     #
 #-:-:-:-:-:-:-::-:-:#
 
 # Author: Kroketaun
-# This module requires XSRFkroke
+# This module requires Lulz-CSRF
 # LulZSeC  AntiSeC
 
 from core import main  # import stuff
