ca8ca9ba2435fffeb717ac36b4b934ff59724bd8
XH·HlulzbinbashH·HX <112975863+lulzbinbashspider@users.noreply.github.com>
2026-06-27 18:48:11 +0200

    Update banner.py

--- core/banner.py
+++ core/banner.py
@@ -11,10 +11,211 @@
 
 # Just for some fancy benner to appear at beginning
 
-from core.colors import *
 import time
+from Lulz-CSRF import __version__
+import Lulz-CSRF.core.colors
+
+colors = Lulz-CSRF.core.colors.color()
+
+SLEEP_TIME = 0
+
 
 def banner():
+    """Display the program banner"""
+    print("\n\n")
+    time.sleep(SLEEP_TIME)
+    print(
+        colors.ORANGE
+        + "     _____       _____       _____      _____       _____                                    "
+    )
+    time.sleep(SLEEP_TIME)
+    print(
+        colors.RED
+        + "  __"
+        + colors.ORANGE
+        + "|"
+        + colors.RED
+        + "__ "
+        + colors.ORANGE
+        + "  |_  "
+        + colors.RED
+        + "__"
+        + colors.ORANGE
+        + "|"
+        + colors.RED
+        + "___ "
+        + colors.ORANGE
+        + " |_  "
+        + colors.RED
+        + "__"
+        + colors.ORANGE
+        + "|"
+        + colors.RED
+        + "___  "
+        + colors.ORANGE
+        + "|_  "
+        + colors.RED
+        + "_"
+        + colors.ORANGE
+        + "|"
+        + colors.RED
+        + "____ "
+        + colors.ORANGE
+        + "|_"
+        + colors.RED
+        + "   _"
+        + colors.ORANGE
+        + "|"
+        + colors.RED
+        + "____ "
+        + colors.ORANGE
+        + "|_ "
+        + colors.RED
+        + " _____   _____  ______  ______  "
+    )
+    time.sleep(SLEEP_TIME)
+    print(
+        colors.RED
+        + " \  `  /    "
+        + colors.ORANGE
+        + "|"
+        + colors.RED
+        + "|   ___|   "
+        + colors.ORANGE
+        + "|"
+        + colors.RED
+        + "|  _  _|   "
+        + colors.ORANGE
+        + "|"
+        + colors.RED
+        + "|   ___|  "
+        + colors.ORANGE
+        + "| "
+        + colors.RED
+        + "|   _  |  "
+        + colors.ORANGE
+        + "|"
+        + colors.RED
+        + "|  _ ,' /     \|  _   )|   ___| "
+    )
+    time.sleep(SLEEP_TIME)
+    print(
+        colors.RED
+        + "  >   <     "
+        + colors.ORANGE
+        + "|"
+        + colors.RED
+        + " `-.`-.    "
+        + colors.ORANGE
+        + "|"
+        + colors.RED
+        + "|     \    "
+        + colors.ORANGE
+        + "|"
+        + colors.RED
+        + "|   ___|  "
+        + colors.ORANGE
+        + "|"
+        + colors.RED
+        + " |    __|  "
+        + colors.ORANGE
+        + "|"
+        + colors.RED
+        + "|     \ |  -  || |_  { |   ___| "
+    )
+    time.sleep(SLEEP_TIME)
+    print(
+        colors.RED
+        + " /__/__\   "
+        + colors.ORANGE
+        + "_|"
+        + colors.RED
+        + "|______|  "
+        + colors.ORANGE
+        + "_|"
+        + colors.RED
+        + "|__|\__\ "
+        + colors.ORANGE
+        + " _|"
+        + colors.RED
+        + "|___|   "
+        + colors.ORANGE
+        + " _|"
+        + colors.RED
+        + " |___|   "
+        + colors.ORANGE
+        + " _|"
+        + colors.RED
+        + "|__|\__\\\_____/|______)|______| "
+    )
+    time.sleep(SLEEP_TIME)
+    print(
+        colors.ORANGE
+        + "    |_____|     |_____|     |_____|    |_____|     |_____| \n\n"
+    )
+    time.sleep(SLEEP_TIME)
+
+
+def banabout():  # some fancy banner stuff :p
+    print(
+        colors.BLUE
+        + "   [---]            "
+        + colors.GREY
+        + "Lulz-CSRF,"
+        + colors.RED
+        + " A"
+        + colors.ORANGE
+        + " Cross Site Request Forgery "
+        + colors.RED
+        + "Audit Toolkit          "
+        + colors.BLUE
+        + "[---]"
+    )
+    time.sleep(SLEEP_TIME)
+    print(
+        colors.BLUE
+        + "   [---]                                                                           [---]"
+    )
+    time.sleep(SLEEP_TIME)
+    print(
+        colors.BLUE
+        + "   [---]   "
+        + colors.PURPLE
+        + "                    "
+        + colors.GREEN
+        + "~  Author : "
+        + colors.CYAN
+        + "Lulzbinbash  ~                   "
+        + colors.BLUE
+        + "     [---]"
+    )
+    time.sleep(SLEEP_TIME)
+    print(
+        colors.BLUE
+        + "   [---]   "
+        + colors.CYAN
+        + "                   ~  github.com / "
+        + colors.GREY
+        + "lulzbinbashider/"
+    time.sleep(SLEEP_TIME)
+    print(
+        colors.BLUE
+        + "   [---]                                                                           [---]"
+    )
+    time.sleep(SLEEP_TIME)
+    print(
+        colors.BLUE
+        + "   [---]  "
+        + colors.ORANGE
+        + "                         ~  Version "
+        + colors.RED
+        + __version__
+        + colors.ORANGE
+        + "  ~                           "
+        + colors.BLUE
+        + "  [---]\n"
+    )
+    time.sleep(SLEEP_TIME)
 
                ``;`                              
              `,+@@@+                              
